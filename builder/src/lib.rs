28f93a70cd7ff81b571147e6b0599c75f15aaa76
destinyfvcker <destinyfunker@gmail.com>
2025-11-19 08:37:31 +0800

    biuild-01: learning DeriveInput

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,7 @@
-use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _intput = parse_macro_input!(input as DeriveInput);
+    proc_macro::TokenStream::new()
 }
